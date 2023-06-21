bb16568cdbe51d268a8f6921546c2c69958ff6bb
wonge1 <edmundwong127@gmail.com>
2023-06-21 02:29:42 -0500

    -added in junit testing

--- Games (3.0)/README.md
+++ Games (3.0)/README.md
@@ -8,7 +8,9 @@ To Do / Ideas List
         -IDEA maybe have points go from 1-26+ for all the different versions of hands 33 vs 44 vs 55 etc
         -look into pulling logic of cards and dupinfo into a super class
 
-
+maybe
 REBUILD DUP COUNT USING CONTAIN METHOD
 
 2. make ai for poker
+
+3. create automatic testing
---
+++ Test/PokerTest.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+class PokerTests {
+
+    private Poker toTest = new Poker(new Player(null),new Computer(),new Scanner(System.in));
+
+    @Test
+    void addition() {
+        //assertEquals(2, calculator.add(1, 1));
+    }
+
+}
